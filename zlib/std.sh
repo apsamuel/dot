eb25673c6d051031c4701abf58436b51cc88ed13
A. Samuel <aaron.psamuel@gmail.com>
2023-11-25 16:38:05 -0500

    chore: update std lib

--- zlib/std.sh
+++ zlib/std.sh
@@ -12,9 +12,19 @@ function emulate::sh () {
     command zsh -c "emulate bash; $code"
 }
 
+function run::sh () {
+    local code="${1:-echo "Hello World"}"
+    command sh -c "$code"
+}
+
 function emulate::bash () {
     local code="${1:-echo "Hello World"}"
     emulate::sh "$code"
+}
+
+function run::bash () {
+    local code="${1:-echo "Hello World"}"
+    command bash -c "$code"
 }
 
 function emulate::csh () {
@@ -22,9 +32,19 @@ function emulate::csh () {
     command zsh -c "emulate csh; $code"
 }
 
-function emulate::kzsh () {
+function run::csh () {
+    local code="${1:-echo "Hello World"}"
+    command csh -c "$code"
+}
+
+function emulate::ksh () {
     local code="${1:-echo "Hello World"}"
     command zsh -c "emulate ksh; $code"
+}
+
+function run::ksh () {
+    local code="${1:-echo "Hello World"}"
+    command ksh -c "$code"
 }
 
 function emulate::zsh () {
