5ad40eb7055b3955ef84730b7991cd744546b738
A. Samuel <aaron.psamuel@gmail.com>
2023-11-29 02:29:22 -0500

    chore: updates

--- zlib/a.sh
+++ zlib/a.sh
@@ -1,3 +1,30 @@
 #!/usr/local/bin/bash
+#% note: this runs first!
+# 🕵️ ignore shellcheck warnings about source statements
+# shellcheck source=/dev/null
 
-echo "a..."
+function __load_secrets__ () {
+    local secret_keys=()
+    # declare -A secrets
+    while IFS=' ' read -r -d ' ' secret_key; do
+        secret_keys+=("${secret_key}")
+    done < <(jq -r '. | keys | .[]' "${ICLOUD}"/dot/secrets.json | xargs) # -print0
+    touch "${TMPDIR}"/.secrets
+    if [[ ${#secret_keys[@]} -gt 0  ]]; then
+        echo "#!/bin/bash" > "$TMPDIR"/.secrets
+    fi
+    for secret_key in "${secret_keys[@]}"; do
+        secret_value="$(jq --arg secret_key "${secret_key}" -r '.[$secret_key]' "${ICLOUD}"/dot/secrets.json)"
+        echo "${secret_key}=${secret_value}" >> "$TMPDIR"/.secrets
+    done
+    if [[ -f "$TMPDIR"/.secrets ]]; then
+        source "$TMPDIR"/.secrets
+        rm -f "$TMPDIR"/.secrets
+    else
+        echo "no secrets file found"
+    fi
+}
+
+function __mask_secrets__ () {
+    true
+}
