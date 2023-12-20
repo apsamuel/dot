f5fed6dc7dac0038c72747bb7d6616c7d2409656
A. Samuel <aaron.psamuel@gmail.com>
2023-12-20 05:13:41 -0500

    chore: updates

--- zlib/base.sh
+++ zlib/base.sh
@@ -1,6 +1,10 @@
 #shellcheck shell=bash
 
+# these values are exported early and available to shells and scripts
+CPU_BRAND="$(sysctl -n machdep.cpu.brand_string)"
+CPU_FEATURES="$(sysctl -n machdep.cpu.features)"
+CPU_CORES="$(sysctl -n machdep.cpu.core_count)"
 OPERATING_SYSTEM=$(uname -s | tr '[:upper:]' '[:lower:]')
-ARCHITECTURE=$(uname -m | tr '[:upper:]' '[:lower:]')
+CPU_ARCHITECTURE=$(uname -m | tr '[:upper:]' '[:lower:]')
 
-export ARCHITECTURE OPERATING_SYSTEM
+export CPU_ARCHITECTURE OPERATING_SYSTEM CPU_BRAND CPU_FEATURES CPU_CORES
