0ad14f769978449018827a34c27fb7d78289679a
A. Samuel <aaron.psamuel@gmail.com>
2023-12-20 13:41:52 -0500

    chore: export bash and zsh versions

--- zlib/base.sh
+++ zlib/base.sh
@@ -6,5 +6,6 @@ CPU_FEATURES="$(sysctl -n machdep.cpu.features)"
 CPU_CORES="$(sysctl -n machdep.cpu.core_count)"
 OPERATING_SYSTEM=$(uname -s | tr '[:upper:]' '[:lower:]')
 CPU_ARCHITECTURE=$(uname -m | tr '[:upper:]' '[:lower:]')
-
-export CPU_ARCHITECTURE OPERATING_SYSTEM CPU_BRAND CPU_FEATURES CPU_CORES
+BASH_RELEASE="$(command bash --version |head -1 | cut -d' ' -f4)"
+ZSH_RELEASE="$(command zsh --version | cut -d' ' -f2)"
+export CPU_ARCHITECTURE OPERATING_SYSTEM CPU_BRAND CPU_FEATURES CPU_CORES BASH_RELEASE ZSH_RELEASE
