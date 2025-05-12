c3ae2d1333876b269b020fa715a0b6288b6ed950
A. Samuel <aaron.psamuel@gmail.com>
2025-05-12 15:41:44 -0400

    chore: adds utility configuration function for Fzf

--- zlib/999-a-terminal.sh
+++ zlib/999-a-terminal.sh
@@ -7,6 +7,21 @@ function compileTerminalInfo() {
 } >/tmp/t && tic -x /tmp/t
 
 # fzf
+function configureFzf() {
+    if  command -v fzf  >/dev/null 2>&1; then
+        # get currently installed version
+        # echo "fzf version: $(brew info fzf --json | jq -r '.[0].linked_keg')"
+        fzf_version="$(brew info fzf --json | jq -r '.[0].linked_keg')"
+        # shellcheck disable=SC1090
+        . "$HOMEBREW_PREFIX"/Cellar/fzf/"${fzf_version}"/shell/completion.zsh || echo "error loading fzf completion"
+
+        # translate bind to bindkey
+        . "$HOMEBREW_PREFIX"/Cellar/fzf/"${fzf_version}"/shell/key-bindings.zsh || echo "error loading fzf key bindings"
+        # source "$HOMEBREW_PREFIX"/Cellar/fzf/"${fzf_version}"/shell/key-bindings.bash
+    fi
+    return 0
+}
+
 if  command -v fzf  >/dev/null 2>&1; then
     # get currently installed version
     # echo "fzf version: $(brew info fzf --json | jq -r '.[0].linked_keg')"
