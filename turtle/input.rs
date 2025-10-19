38f2fb156e05d1875fb6c39d4d023a5625bb6396
apsamuel <aaron.psamuel@gmail.com>
2025-10-19 04:18:35 -0400

    fix: resolve many warnings

--- turtle/input.rs
+++ turtle/input.rs
@@ -20,3 +20,23 @@ pub fn expand_tilde(input: &str) -> String {
     }
     input.to_string()
 }
+
+pub fn expand_single_dot(input: &str) -> String {
+    if input.starts_with("./") || input == "." {
+        if let Ok(current_dir) = env::current_dir() {
+            return input.replacen(".", current_dir.to_str().unwrap(), 1);
+        }
+    }
+    input.to_string()
+}
+
+pub fn expand_double_dot(input: &str) -> String {
+    if input.starts_with("../") || input == ".." {
+        if let Ok(current_dir) = env::current_dir() {
+            if let Some(parent) = current_dir.parent() {
+                return input.replacen("..", parent.to_str().unwrap(), 1);
+            }
+        }
+    }
+    input.to_string()
+}
