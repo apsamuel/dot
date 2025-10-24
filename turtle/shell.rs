252fb44a4bfc1d355045d0540131e723be209ce1
apsamuel <aaron.psamuel@gmail.com>
2025-10-24 14:04:33 -0400

    fix(turtle::shell): address warnings

--- turtle/shell.rs
+++ turtle/shell.rs
@@ -1,22 +1,22 @@
 #[derive(Debug, Clone)]
 pub struct TurtleShell {
-    pub debug: bool,
-    history: Vec<crate::types::HistoryEvent>,
-    env: std::collections::HashMap<String, String>,
-    aliases: std::collections::HashMap<String, String>,
-    pub config: Option<crate::types::TurtleConfig>,
-    pub args: Option<crate::types::TurtleArgs>,
-    pub interpreter: crate::interpreter::TurtleInterpreter,
-    pub pid: Option<u32>,
-    pub uptime: u64,
-    pub paused: bool,
-    pub running: bool,
+    pub _debug: bool,
+    _history: Vec<crate::types::HistoryEvent>,
+    _env: std::collections::HashMap<String, String>,
+    _aliases: std::collections::HashMap<String, String>,
+    pub _config: Option<crate::types::TurtleConfig>,
+    pub _args: Option<crate::types::TurtleArgs>,
+    pub _interpreter: crate::interpreter::TurtleInterpreter,
+    pub _pid: Option<u32>,
+    pub _uptime: u64,
+    pub _paused: bool,
+    pub _running: bool,
 }
 
 impl TurtleShell {
-    fn reload(&self) -> crate::types::TurtleConfig {
+    fn _reload(&self) -> crate::types::TurtleConfig {
         // Load configuration from file or environment variables
-        crate::config::load_config(self.config.as_ref().unwrap().debug).unwrap_or_else(|| {
+        crate::config::load_config(self._config.as_ref().unwrap().debug).unwrap_or_else(|| {
             crate::types::TurtleConfig {
                 // history: None,
                 debug: false,
@@ -28,7 +28,7 @@ impl TurtleShell {
         })
     }
 
-    fn read_input(&self) -> String {
+    fn _read_input(&self) -> String {
         // Read input from user
         String::new() // Placeholder
     }
@@ -50,30 +50,30 @@ impl TurtleShell {
         // if
 
         TurtleShell {
-            config,
-            args,
-            debug,
-            history,
-            env,
-            aliases,
-            interpreter,
-            pid: None,
-            uptime: 0,
-            paused: false,
-            running: true,
+            _config: config,
+            _args: args,
+            _debug: debug,
+            _history: history,
+            _env: env,
+            _aliases: aliases,
+            _interpreter: interpreter,
+            _pid: None,
+            _uptime: 0,
+            _paused: false,
+            _running: true,
             // debug,
         }
     }
 
-    pub fn start(&mut self) {
+    pub fn _start(&mut self) {
         // TODO: implement main shell loop here
         let _start = crate::utils::now_unix();
         loop {
-            if !self.running {
+            if !self._running {
                 break;
             }
 
-            if !self.paused {
+            if !self._paused {
                 // Read input, parse, execute commands, etc.
             }
             // shell logic here
