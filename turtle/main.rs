3ff680631a4c0d636a7c28f083abf8514bda1462
apsamuel <aaron.psamuel@gmail.com>
2025-10-30 17:49:58 -0400

    chore: updates...

--- turtle/main.rs
+++ turtle/main.rs
@@ -1,14 +1,14 @@
-mod config; // configuration loading and environment setup
+mod configuration; // configuration loading and environment setup
 mod defaults; // default configuration values
 mod execution; // execution context
-mod history; // command history logging
-mod input; // input handling (env var and tilde expansion)
+// mod history; // command history logging
+// mod input; // input handling (env var and tilde expansion)
 mod interpreter; // turtle shell interpreter
 mod shell;
 mod types; // shared structs/enums
 mod utils; // utility functions // main shell struct
 
-mod builtin; // built-in commands
+// mod _builtin; // built-in commands
 mod themes;
 mod widgets; // TUI widgets // theme management
 
@@ -18,8 +18,8 @@ use clap::Parser; // command line argument parsing
 
 #[tokio::main]
 async fn main() {
-    let args = crate::types::TurtleArgs::parse();
-    let mut shell = crate::shell::TurtleShell::new(args.clone());
+    let args = crate::types::Arguments::parse();
+    let mut shell = crate::shell::Shell::new(args.clone());
 
     loop {
         shell.start();
