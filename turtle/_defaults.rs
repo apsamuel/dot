ab602b1a52d36a664d5d97bbdba1d2f9be6edacb
apsamuel <aaron.psamuel@gmail.com>
2025-10-31 16:12:08 -0400

    chore: move defaults to types

---
+++ turtle/_defaults.rs
@@ -0,0 +1,12 @@
+// use serde::{Deserialize, Serialize};
+
+// // pub static DEFAULT_CONFIG_PATH: &str = "~/.turtlerc.yaml";
+// // pub static DEFAULT_HISTORY_PATH: &str = "~/.turtle_history.json";
+// pub static DEFAULT_PROMPT: &str = "<<🐢>> ";
+// pub static DEFAULT_HISTORY_SIZE: usize = 1000;
+// pub static DEFAULT_THEME: &str = "solarized_dark";
+// pub static DEFAULT_DEBUG: bool = false;
+
+// pub fn get_defaults() -> TurtleDefaults {
+//     TurtleDefaults::default()
+// }
