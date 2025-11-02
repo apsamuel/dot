fdfe44bc4bf301bd3e3a78d2058626a145dc15f7
apsamuel <aaron.psamuel@gmail.com>
2025-11-02 04:25:21 -0500

    feat: other deprecated modules...

--- turtle/themes.rs
+++ turtle/themes.rs
@@ -1,146 +1,146 @@
-use crossterm::execute;
-use crossterm::style::{
-    Color,
-    ResetColor,
-    // Attribute
-    SetBackgroundColor,
-    SetForegroundColor,
-};
-use once_cell::sync::Lazy;
-use std::collections::HashMap;
-use std::io::{self, Write};
+// use crossterm::execute;
+// use crossterm::style::{
+//     Color,
+//     ResetColor,
+//     // Attribute
+//     SetBackgroundColor,
+//     SetForegroundColor,
+// };
+// use once_cell::sync::Lazy;
+// use std::collections::HashMap;
+// use std::io::{self, Write};
 
-static DEFAULT_THEME: &str = "catppuccino";
+// static DEFAULT_THEME: &str = "catppuccino";
 
-static SOLARIZED_DARK: crate::types::Theme = crate::types::Theme {
-    foreground: Color::Rgb {
-        r: 131,
-        g: 148,
-        b: 150,
-    },
-    background: Color::Rgb { r: 0, g: 43, b: 54 },
-    text: Color::Rgb {
-        r: 147,
-        g: 161,
-        b: 161,
-    },
-    cursor: Color::Rgb {
-        r: 147,
-        g: 161,
-        b: 161,
-    },
-    selection: Color::Rgb { r: 7, g: 54, b: 66 },
-    // attributes: &["bold", "italic"],
-};
+// static SOLARIZED_DARK: crate::types::Theme = crate::types::Theme {
+//     foreground: Color::Rgb {
+//         r: 131,
+//         g: 148,
+//         b: 150,
+//     },
+//     background: Color::Rgb { r: 0, g: 43, b: 54 },
+//     text: Color::Rgb {
+//         r: 147,
+//         g: 161,
+//         b: 161,
+//     },
+//     cursor: Color::Rgb {
+//         r: 147,
+//         g: 161,
+//         b: 161,
+//     },
+//     selection: Color::Rgb { r: 7, g: 54, b: 66 },
+//     // attributes: &["bold", "italic"],
+// };
 
-static SOLARIZED_LIGHT: crate::types::Theme = crate::types::Theme {
-    foreground: Color::Rgb {
-        r: 101,
-        g: 123,
-        b: 131,
-    },
-    background: Color::Rgb {
-        r: 253,
-        g: 246,
-        b: 227,
-    },
-    text: Color::Rgb {
-        r: 101,
-        g: 123,
-        b: 131,
-    },
-    cursor: Color::Rgb {
-        r: 101,
-        g: 123,
-        b: 131,
-    },
-    selection: Color::Rgb {
-        r: 238,
-        g: 232,
-        b: 213,
-    },
-    // attributes: &["bold", "italic"],
-};
+// static SOLARIZED_LIGHT: crate::types::Theme = crate::types::Theme {
+//     foreground: Color::Rgb {
+//         r: 101,
+//         g: 123,
+//         b: 131,
+//     },
+//     background: Color::Rgb {
+//         r: 253,
+//         g: 246,
+//         b: 227,
+//     },
+//     text: Color::Rgb {
+//         r: 101,
+//         g: 123,
+//         b: 131,
+//     },
+//     cursor: Color::Rgb {
+//         r: 101,
+//         g: 123,
+//         b: 131,
+//     },
+//     selection: Color::Rgb {
+//         r: 238,
+//         g: 232,
+//         b: 213,
+//     },
+//     // attributes: &["bold", "italic"],
+// };
 
-static MONOKAI: crate::types::Theme = crate::types::Theme {
-    foreground: Color::Rgb {
-        r: 248,
-        g: 248,
-        b: 242,
-    },
-    background: Color::Rgb {
-        r: 39,
-        g: 40,
-        b: 34,
-    },
-    text: Color::Rgb {
-        r: 248,
-        g: 248,
-        b: 242,
-    },
-    cursor: Color::Rgb {
-        r: 248,
-        g: 248,
-        b: 242,
-    },
-    selection: Color::Rgb {
-        r: 73,
-        g: 72,
-        b: 62,
-    },
-    // attributes: &["bold"],
-};
+// static MONOKAI: crate::types::Theme = crate::types::Theme {
+//     foreground: Color::Rgb {
+//         r: 248,
+//         g: 248,
+//         b: 242,
+//     },
+//     background: Color::Rgb {
+//         r: 39,
+//         g: 40,
+//         b: 34,
+//     },
+//     text: Color::Rgb {
+//         r: 248,
+//         g: 248,
+//         b: 242,
+//     },
+//     cursor: Color::Rgb {
+//         r: 248,
+//         g: 248,
+//         b: 242,
+//     },
+//     selection: Color::Rgb {
+//         r: 73,
+//         g: 72,
+//         b: 62,
+//     },
+//     // attributes: &["bold"],
+// };
 
-static CATPPUCCINO: crate::types::Theme = crate::types::Theme {
-    foreground: Color::Rgb {
-        r: 75,
-        g: 56,
-        b: 50,
-    },
-    background: Color::Rgb {
-        r: 241,
-        g: 224,
-        b: 214,
-    },
-    text: Color::Rgb {
-        r: 75,
-        g: 56,
-        b: 50,
-    },
-    cursor: Color::Rgb {
-        r: 75,
-        g: 56,
-        b: 50,
-    },
-    selection: Color::Rgb {
-        r: 224,
-        g: 200,
-        b: 176,
-    },
-    // attributes: &["italic"],
-};
+// static CATPPUCCINO: crate::types::Theme = crate::types::Theme {
+//     foreground: Color::Rgb {
+//         r: 75,
+//         g: 56,
+//         b: 50,
+//     },
+//     background: Color::Rgb {
+//         r: 241,
+//         g: 224,
+//         b: 214,
+//     },
+//     text: Color::Rgb {
+//         r: 75,
+//         g: 56,
+//         b: 50,
+//     },
+//     cursor: Color::Rgb {
+//         r: 75,
+//         g: 56,
+//         b: 50,
+//     },
+//     selection: Color::Rgb {
+//         r: 224,
+//         g: 200,
+//         b: 176,
+//     },
+//     // attributes: &["italic"],
+// };
 
-static THEMES: Lazy<HashMap<&'static str, &'static crate::types::Theme>> = Lazy::new(|| {
-    let mut m = HashMap::new();
-    m.insert("solarized_dark", &SOLARIZED_DARK);
-    m.insert("solarized_light", &SOLARIZED_LIGHT);
-    m.insert("monokai", &MONOKAI);
-    m.insert("catppuccino", &CATPPUCCINO);
-    m
-});
+// static THEMES: Lazy<HashMap<&'static str, &'static crate::types::Theme>> = Lazy::new(|| {
+//     let mut m = HashMap::new();
+//     m.insert("solarized_dark", &SOLARIZED_DARK);
+//     m.insert("solarized_light", &SOLARIZED_LIGHT);
+//     m.insert("monokai", &MONOKAI);
+//     m.insert("catppuccino", &CATPPUCCINO);
+//     m
+// });
 
-pub fn apply_theme<W: Write>(writer: &mut W, theme_name: &str) -> io::Result<()> {
-    let theme = THEMES.get(theme_name).or_else(|| THEMES.get(DEFAULT_THEME));
-    let theme = match theme {
-        Some(theme) => theme,
-        None => return Err(io::Error::new(io::ErrorKind::NotFound, "Theme not found")),
-    };
+// pub fn apply_theme<W: Write>(writer: &mut W, theme_name: &str) -> io::Result<()> {
+//     let theme = THEMES.get(theme_name).or_else(|| THEMES.get(DEFAULT_THEME));
+//     let theme = match theme {
+//         Some(theme) => theme,
+//         None => return Err(io::Error::new(io::ErrorKind::NotFound, "Theme not found")),
+//     };
 
-    execute!(
-        writer,
-        ResetColor,
-        SetForegroundColor(theme.foreground),
-        SetBackgroundColor(theme.background),
-    )?;
-    Ok(())
-}
+//     execute!(
+//         writer,
+//         ResetColor,
+//         SetForegroundColor(theme.foreground),
+//         SetBackgroundColor(theme.background),
+//     )?;
+//     Ok(())
+// }
