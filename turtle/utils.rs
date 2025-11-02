23f1594cffaed6ada818a3a13d054f9ea05c831e
apsamuel <aaron.psamuel@gmail.com>
2025-11-02 04:25:03 -0500

    fix: fixes a bug in the expand_path function, which caused paths with a leading tilde to not properly be rendered. This only showed when I started to use the default path for the history file.

--- turtle/utils.rs
+++ turtle/utils.rs
@@ -12,6 +12,21 @@ pub fn this_instant() -> std::time::Instant {
     std::time::Instant::now()
 }
 
+pub fn expand_path(path: &str) -> String {
+    // Expand ~ to home directory
+    if path.starts_with("~/") {
+        if let Some(home) = dirs::home_dir() {
+            let without_tilde = &path[2..]; // Remove "~/"
+            return home.join(without_tilde).to_string_lossy().to_string();
+        }
+    } else if path == "~" {
+        if let Some(home) = dirs::home_dir() {
+            return home.to_string_lossy().to_string();
+        }
+    }
+    path.to_string()
+}
+
 /// Check if the given path exists and is a file or directory
 pub fn is_path(path: &str) -> bool {
     let p = std::path::Path::new(path);
@@ -48,22 +63,22 @@ pub fn is_command(command: &str) -> bool {
     false
 }
 
-pub fn translate_alias(
-    aliases: &std::collections::HashMap<String, String>,
-    input: &str,
-) -> Option<String> {
-    let parts: Vec<&str> = input.split_whitespace().collect();
-    if let Some(alias_command) = aliases.get(parts[0]) {
-        let mut command = alias_command.clone();
-        if parts.len() > 1 {
-            command.push(' ');
-            command.push_str(&parts[1..].join(" "));
-        }
-        Some(command)
-    } else {
-        None
-    }
-}
+// pub fn translate_alias(
+//     aliases: &std::collections::HashMap<String, String>,
+//     input: &str,
+// ) -> Option<String> {
+//     let parts: Vec<&str> = input.split_whitespace().collect();
+//     if let Some(alias_command) = aliases.get(parts[0]) {
+//         let mut command = alias_command.clone();
+//         if parts.len() > 1 {
+//             command.push(' ');
+//             command.push_str(&parts[1..].join(" "));
+//         }
+//         Some(command)
+//     } else {
+//         None
+//     }
+// }
 
 pub fn build_user_environment() -> std::collections::HashMap<String, String> {
     let mut details = std::collections::HashMap::new();
@@ -75,4 +90,46 @@ pub fn build_user_environment() -> std::collections::HashMap<String, String> {
         details.insert("HOME".to_string(), home_dir.to_string_lossy().to_string());
     }
     return details;
+}
+
+pub fn load_history_events(path: &str) -> Option<Vec<crate::types::Event>> {
+    println!("Loading history events from: {}", path);
+    let data = std::fs::read_to_string(path).ok()?;
+    let mut events = Vec::new();
+    for line in data.lines() {
+        let event: crate::types::Event = serde_json::from_str(line).ok()?;
+        events.push(event);
+    }
+    print!("Loaded {} history events", events.len());
+
+    Some(events)
+}
+
+/// Set default shell environment variables
+pub fn set_shell_vars() {
+    // Set SHELL to the path of the running binary
+    if let Ok(exe_path) = std::env::current_exe() {
+        if let Some(path_str) = exe_path.to_str() {
+            unsafe {
+                std::env::set_var("SHELL", path_str);
+            }
+        }
+    }
+}
+
+/// Load aliases from configuration into the provided aliases map
+pub fn load_aliases(
+    aliases: &mut std::collections::HashMap<String, String>,
+    config: &Option<crate::types::Config>,
+) -> std::collections::HashMap<String, String> {
+    let turtle_aliases = if let Some(cfg) = config {
+        cfg.aliases.clone().unwrap_or_default()
+    } else {
+        std::collections::HashMap::new()
+    };
+    for (key, value) in turtle_aliases {
+        aliases.insert(key, value);
+    }
+
+    aliases.clone()
 }
