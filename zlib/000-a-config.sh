dfffe1f7689fcfb9f06e96cb67082368101d1c95
apsamuel <aaron.psamuel@gmail.com>
2026-04-10 04:20:21 -0400

    chore: minor updates

--- zlib/000-a-config.sh
+++ zlib/000-a-config.sh
@@ -1,6 +1,13 @@
 #shellcheck shell=bash
 #% description: base variables and functions
 
+# Prevent pagers from switching to alternate screen buffer.
+# -R: pass ANSI colors through  -X: skip terminal init/deinit (no alt buffer)  -F: quit if output fits on one screen
+export LESS="-R -X -F"
+export BAT_PAGER="less -R -X -F"
+export GIT_PAGER="less -R -X -F"
+export MANPAGER="less -R -X -F"
+
 icloud_directory="${HOME}/Library/Mobile Documents/com~apple~CloudDocs"
 icloud_dot_directory="${icloud_directory}/dot"
 
