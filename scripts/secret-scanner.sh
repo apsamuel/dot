b13353ad1a30b63573445f4e68f2b1ce905d76d1
apsamuel <aaron.psamuel@gmail.com>
2026-05-05 09:48:55 -0400

    fix(secret-scanner): remove unnecessary results flag from TruffleHog command

--- scripts/secret-scanner.sh
+++ scripts/secret-scanner.sh
@@ -5,6 +5,5 @@ podman run --rm \
   -v "$HOME"/.dot:/src \
   trufflesecurity/trufflehog:latest \
   git file:///src \
-  --results=verified,unknown \
   --fail \
   --exclude-paths /src/.trufflehog-exclude.txt
